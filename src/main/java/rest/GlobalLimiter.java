9f4e9ff2cb4033b5e1f1759b98cb3b1e410670eb
MissterP <pablo.aparici.contact@gmail.com>
2024-12-05 20:54:20 +0100

    Remove unused RestInvoker from utils. Add GlobalLimiter class to limit number requests

---
+++ src/main/java/rest/GlobalLimiter.java
@@ -0,0 +1,29 @@
+package rest;
+
+import java.util.concurrent.Semaphore;
+import com.google.common.util.concurrent.RateLimiter;
+
+public class GlobalLimiter {
+    private static final GlobalLimiter instance = new GlobalLimiter(); // Static instance to share the same semaphore and rate limiter for all RESTInvoker instances.
+
+    private final Semaphore semaphore;
+    private final RateLimiter rateLimiter;
+
+    private GlobalLimiter() {
+        semaphore = new Semaphore(100); // Max 100 requests at the same time.
+        rateLimiter = RateLimiter.create(100.0); // Max 100 requests per second.
+    }
+
+    public static GlobalLimiter getInstance() {
+        return instance;
+    }
+
+    public void acquire() throws InterruptedException {
+        rateLimiter.acquire(); 
+        semaphore.acquire();   
+    }
+
+    public void release() {
+        semaphore.release();
+    }
+}
